5ee258b045879afd8baf7a7b34b74fde9be14076
Surya-SR <suryamicroservices@gmail.com>
2024-06-15 23:27:48 +0530

    add terraform config files to create EC2

---
+++ EC2/main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "ec2" {
+  ami = var.ami_id
+  associate_public_ip_address = var.associate_public_ip_address
+  availability_zone = var.availability_zone
+  subnet_id = var.subnet_id
+  instance_type = var.instance_type
+  key_name = var.key_name
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    delete_on_termination = true
+    encrypted = true
+    volume_size = 20
+  }
+
+}
---
+++ EC2/variables.tf
@@ -0,0 +1,27 @@
+variable "ami_id" {
+  
+}
+
+variable "vpc_security_group_ids" {
+  
+}
+
+variable "associate_public_ip_address" {
+  
+}
+
+variable "availability_zone" {
+  
+}
+
+variable "subnet_id" {
+  
+}
+
+variable "instance_type" {
+  
+}
+
+variable "key_name" {
+  
+}
